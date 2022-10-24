65b2a4c6057abb14c7de54f52e37468a383eb60e
César A <caesar.augustusqp@gmail.com>
2022-10-24 18:02:52 +0200

    Estructura de la API

---
+++ api/api.js
@@ -0,0 +1,22 @@
+-- Send job ID
+-- Meter en cola trabajo dando una id
+-- input: Nueva ID
+timestamp + uuid
+
+
+-- Return job ID
+-- Devolver resultado al cliente
+
+
+-- Question status
+-- input: ID
+
+
+-- Get result
+-- Frontend pide el resultado al worker
+-- input: ID
+
+
+-- Get list of jobs
+-- Cliente pregunta por sus trabajos
+
