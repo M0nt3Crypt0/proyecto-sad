4d676cc6c80f5012e5de6bf05542217ca485d2ef
César A <caesar.augustusqp@gmail.com>
2022-10-28 16:27:01 +0200

    Cambiar server de net a zmq

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,9 +1,20 @@
+// Librerías
 const zmq = require('zeromq');
 
-
+// Puertos y objetos
 const FRONTPORT = 9998;
 const BACKPORT = 9999;
 
+// Declaración de los sockets 
 
-const server = net.CreateServer(onClientConnected);
-server.listen(FRONTPORT);
+// Socket cliente (pub-sub(???))
+
+
+
+// Socket broker-kafka (
+
+
+
+// Métodos
+
+
