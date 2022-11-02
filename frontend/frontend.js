78b88e8f48c500745deeada2562d39b3a8556c5e
César A <caesar.augustusqp@gmail.com>
2022-11-02 20:59:18 +0100

    Estructura de frontend

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -11,10 +11,32 @@ const BACKPORT = 9999;
 
 
 
-// Socket broker-kafka (
+// Socket broker-kafka (router-router???)
 
 
 
 // Métodos
+// Cuando se conecta un cliente envia el trabajo a la cola y le da una ID 
+function SendJob() {
 
+	return id;
+}
 
+function GetStatus(String id) {
+	let status;
+
+	return status;
+}
+
+// Delete from queue
+function GetResult(String id) {
+	let res;
+
+	return res;
+}
+
+function ListJobs(String cliente) {
+	let jobs;
+
+	return jobs
+}
