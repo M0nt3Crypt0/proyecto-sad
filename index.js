977cae1049077281a097d31ee096c90842accb87
César A <caesar.augustusqp@gmail.com>
2022-12-16 16:22:42 +0100

    Cambiada estructura de carpetas

---
+++ index.js
@@ -0,0 +1,18 @@
+// Librerías
+const express = require('express');
+const app = express();
+
+// Puertos y objetos
+const FRONTPORT = 9998;
+const BACKPORT = 9999;
+
+// Controlador con REST API
+
+const 
+
+// 
+app.listen(FRONTPORT, () => {
+	console.log("Server en puerto " + FRONTPORT);
+});
+
+
